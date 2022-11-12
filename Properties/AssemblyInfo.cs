030a65be4fccbb5e3e164a0491c18d9b6226c7c5
Tofuman <d.worth@btinternet.com>
2022-11-12 22:02:00 +0000

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PRC")]
+[assembly: AssemblyDescription("C# version of the PRC ciphering code used in PSO")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tofuman")]
+[assembly: AssemblyProduct("PRC")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("89490915-a721-44e5-8a2a-f26db99c25fd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# PRC C# Library #
+
+A C# version of my C++ PRC cipher code.
+
+Used in some PSO PRS files to add an additional level of protection.
